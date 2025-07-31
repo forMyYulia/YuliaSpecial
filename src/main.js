f4de6b3d3c5b1944130bec48e2ed43a08b79ac15
SkakunOstap <skakunostap@gmail.com>
2025-07-31 03:08:16 +0300

    Mobile fixed p1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 const box = document.querySelector('#box')
+const blackCurtain = document.querySelector('#blackCurtain')
 const underText = document.querySelector('#underText')
 const top = document.querySelector('#boxSideTop')
 const bottomTexts = document.querySelectorAll('.boxSideBottomText')
@@ -42,29 +43,33 @@ for (let i = 0; i < bottomTexts.length; i++) {
 // box.style.opacity = '1';
 top.style.opacity = '1';
 
+window.onload = () => {
+    blackCurtain.style.opacity = '0';
+}
+
 window.addEventListener('scroll', (e) => {
-    underText.style.transform = `scale(${Math.min(window.scrollY / 15, 150) / 100}) translateY(-${Math.min(Math.max(window.scrollY/20-750, 0), 135)}px)`
-    box.style.transform = `scale(${Math.max(180-window.scrollY/15, 80)/100})`// rotate(${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
+    underText.style.transform = `scale(${Math.min(window.scrollY / 15, 150) / 100}) translateY(-${Math.min(Math.max(window.scrollY/15-720, 0), 135)}px)`
+    box.style.transform = `scale(${Math.max(160-window.scrollY/15, 60)/100})`// rotate(${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
     top.style.transform = `translateY(-${window.scrollY/2}px) rotate(${window.scrollY/20}deg)`;
     // bottomText.style.transform = `rotate(-${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
     for (let i = 0; i < 4; i++) {
         sides[i].style.transform = `rotate${i < 2 ? 'Y': 'X'}(${i === 2 ? -Math.max(90 - window.scrollY/10, -90) : ((i%2) * 2 - 1)*Math.max(90 - window.scrollY/20, 0)}deg)`;
         if (i < 2)
-            subSides[i].style.transform = `translateY(${Math.min(window.scrollY/6, 289)}px) translateX(${i === 1 || i === 2 ? '': '-'}${Math.max(Math.min((window.scrollY-8500)/120, 19), 0)*10}px)`;
+            subSides[i].style.transform = `translateY(${Math.min(window.scrollY/6, 289)}px) translateX(${i === 1 || i === 2 ? '': '-'}${Math.max(Math.min((window.scrollY-6300)/120, 19), 0)*10}px)`;
     }
     for (let j = 2; j < 6; j++) {
-        subSides[j].style.transform = `translateX(${j < 4 ? '-': ''}${Math.min(window.scrollY/6, 289)}px) translateY(${j === 3 || j === 4 ? '': '-'}${Math.max(Math.min((window.scrollY-4500)/120, 19), 0)*10}px)`;
+        subSides[j].style.transform = `translateX(${j < 4 ? '-': ''}${Math.min(window.scrollY/6, 289)}px) translateY(${j === 3 || j === 4 ? '': '-'}${Math.max(Math.min((window.scrollY-3000)/120, 19), 0)*10}px)`;
     }
     for (let j = 6; j < 9; j++) {
-        subSides[j].style.transform = `translate${j < 8? 'X' : 'Y'}(${j === 7 ? '-': ''}${Math.min(window.scrollY/6, 289)+Math.max(Math.min((window.scrollY-13000)/120, 13), 0)*10}px)`;
+        subSides[j].style.transform = `translate${j < 8? 'X' : 'Y'}(${j === 7 ? '-': ''}${Math.min(window.scrollY/6, 289)+Math.max(Math.min((window.scrollY-9800)/120, 13), 0)*10}px)`;
     }
     // bottomTexts[0].style.filter = `blur(${Math.min(Math.max(window.scrollY/20-140, 0), 150)}px)`;
     // bottomTexts[1].style.filter = `blur(${Math.min(Math.max(150 - Math.max(window.scrollY/20-50, 0), 0) + 2*Math.max(window.scrollY/20-270, 0), 150)}px)`;
     // bottomTexts[2].style.filter = `blur(${Math.min(Math.max(150 - Math.max(window.scrollY/20-150, 0), 0) + 2*Math.max(window.scrollY/20-370, 0), 150)}px)`;
     // bottomTexts[3].style.filter = `blur(${Math.min(Math.max(150 - Math.max(window.scrollY/20-250, 0), 0), 150)}px)`;
 
-    bottomTexts[0].style.opacity = `${100-Math.min(Math.max(window.scrollY/20-150, 0), 100)}%`;
-    bottomTexts[1].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/20-200, 0), 0) + 2*Math.max(window.scrollY/20-400, 0), 100)}%`;
-    bottomTexts[2].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/20-400, 0), 0) + 2*Math.max(window.scrollY/20-600, 0), 100)}%`;
-    bottomTexts[3].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/20-600, 0), 0), 100)}%`;
+    bottomTexts[0].style.opacity = `${100-Math.min(Math.max(window.scrollY/15-150, 0), 100)}%`;
+    bottomTexts[1].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/15-200, 0), 0) + 2*Math.max(window.scrollY/15-400, 0), 100)}%`;
+    bottomTexts[2].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/15-400, 0), 0) + 2*Math.max(window.scrollY/15-600, 0), 100)}%`;
+    bottomTexts[3].style.opacity = `${100-Math.min(Math.max(150 - Math.max(window.scrollY/15-600, 0), 0), 100)}%`;
 })
