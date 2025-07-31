e1c052604ff58f149494c8f0de0ee57456e6f873
SkakunOstap <skakunostap@gmail.com>
2025-07-31 03:38:55 +0300

    Mobile fixed p2

--- src/main.js
+++ src/main.js
@@ -27,10 +27,10 @@ const subSides = [
 
 for (let i = 0; i < sides.length+subSides.length; i++) {
     if (i < 4 && i !== 2)
-        sides[i].innerHTML = `<div class=\"relative\"><img src=\"./img${i+1}.jpg\" alt=\"<3\" class=\"max-h-52 max-w-52\"><img src=\"./corner.png\" class=\"absolute -top-2 -left-2 w-8 h-8\"><img src=\"./corner.png\" class=\"absolute -top-2 -right-2 w-8 h-8 -scale-x-100\"><img src=\"./corner.png\" class=\"absolute -bottom-2 -left-2 w-8 h-8 -scale-y-100\"><img src=\"./corner.png\" class=\"absolute -bottom-2 -right-2 w-8 h-8 -scale-x-100 -scale-y-100\"></div>`
+        sides[i].innerHTML = `<div class=\"relative\"><img src=\"./img${i+1}.jpg\" alt=\"<3\" class=\"max-h-24 max-w-24\"><img src=\"./corner.png\" class=\"absolute -top-1 -left-1 w-4 h-4\"><img src=\"./corner.png\" class=\"absolute -top-1 -right-1 w-4 h-4 -scale-x-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -left-1 w-4 h-4 -scale-y-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -right-1 w-4 h-4 -scale-x-100 -scale-y-100\"></div>`
     else if (i !== 2) {
-        subSides[i - 4].classList.add("size-36");
-        subSides[i - 4].innerHTML = `<div class=\"relative\"><img src=\"./img${i + 1}.jpg\" alt=\"<3\" class=\"max-h-30 max-w-30\"><img src=\"./corner.png\" class=\"absolute -top-1 -left-1 w-4 h-4\"><img src=\"./corner.png\" class=\"absolute -top-1 -right-1 w-4 h-4 -scale-x-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -left-1 w-4 h-4 -scale-y-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -right-1 w-4 h-4 -scale-x-100 -scale-y-100\"></div>`;
+        subSides[i - 4].classList.add("size-16");
+        subSides[i - 4].innerHTML = `<div class=\"relative\"><img src=\"./img${i + 1}.jpg\" alt=\"<3\" class=\"max-h-12 max-w-12\"><img src=\"./corner.png\" class=\"absolute -top-1 -left-1 w-3 h-3\"><img src=\"./corner.png\" class=\"absolute -top-1 -right-1 w-3 h-3 -scale-x-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -left-1 w-3 h-3 -scale-y-100\"><img src=\"./corner.png\" class=\"absolute -bottom-1 -right-1 w-3 h-3 -scale-x-100 -scale-y-100\"></div>`;
     }
 }
 
@@ -48,20 +48,20 @@ window.onload = () => {
 }
 
 window.addEventListener('scroll', (e) => {
-    underText.style.transform = `scale(${Math.min(window.scrollY / 15, 150) / 100}) translateY(-${Math.min(Math.max(window.scrollY/15-720, 0), 135)}px)`
-    box.style.transform = `scale(${Math.max(160-window.scrollY/15, 60)/100})`// rotate(${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
+    underText.style.transform = `scale(${Math.min(window.scrollY / 15, 150) / 100}) translateY(-${Math.min(Math.max(window.scrollY/15-720, 0), 65)}px)`
+    box.style.transform = `scale(${Math.max(200-window.scrollY/15, 100)/100})`// rotate(${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
     top.style.transform = `translateY(-${window.scrollY/2}px) rotate(${window.scrollY/20}deg)`;
     // bottomText.style.transform = `rotate(-${Math.max(Math.min((window.scrollY-2500)/80, 20), 0)}deg)`;
     for (let i = 0; i < 4; i++) {
         sides[i].style.transform = `rotate${i < 2 ? 'Y': 'X'}(${i === 2 ? -Math.max(90 - window.scrollY/10, -90) : ((i%2) * 2 - 1)*Math.max(90 - window.scrollY/20, 0)}deg)`;
         if (i < 2)
-            subSides[i].style.transform = `translateY(${Math.min(window.scrollY/6, 289)}px) translateX(${i === 1 || i === 2 ? '': '-'}${Math.max(Math.min((window.scrollY-6300)/120, 19), 0)*10}px)`;
+            subSides[i].style.transform = `translateY(${Math.min(window.scrollY/12, 148)}px) translateX(${i === 1 || i === 2 ? '': '-'}${Math.max(Math.min((window.scrollY-6300)/120, 9), 0)*10}px)`;
     }
     for (let j = 2; j < 6; j++) {
-        subSides[j].style.transform = `translateX(${j < 4 ? '-': ''}${Math.min(window.scrollY/6, 289)}px) translateY(${j === 3 || j === 4 ? '': '-'}${Math.max(Math.min((window.scrollY-3000)/120, 19), 0)*10}px)`;
+        subSides[j].style.transform = `translateX(${j < 4 ? '-': ''}${Math.min(window.scrollY/12, 148)}px) translateY(${j === 3 || j === 4 ? '': '-'}${Math.max(Math.min((window.scrollY-3000)/120, 9), 0)*10}px)`;
     }
     for (let j = 6; j < 9; j++) {
-        subSides[j].style.transform = `translate${j < 8? 'X' : 'Y'}(${j === 7 ? '-': ''}${Math.min(window.scrollY/6, 289)+Math.max(Math.min((window.scrollY-9800)/120, 13), 0)*10}px)`;
+        subSides[j].style.transform = `translate${j < 8? 'X' : 'Y'}(${j === 7 ? '-': ''}${Math.min(window.scrollY/12, 148)+Math.max(Math.min((window.scrollY-9800)/120, 6), 0)*10}px)`;
     }
     // bottomTexts[0].style.filter = `blur(${Math.min(Math.max(window.scrollY/20-140, 0), 150)}px)`;
     // bottomTexts[1].style.filter = `blur(${Math.min(Math.max(150 - Math.max(window.scrollY/20-50, 0), 0) + 2*Math.max(window.scrollY/20-270, 0), 150)}px)`;
